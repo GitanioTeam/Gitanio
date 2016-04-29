a98a2612adf66a62d9fd8e02a75ebd3ba419022b
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2016-04-29 20:59:32 -0300

    Estrutura inicial do projeto utilizando spring boot/gradle

---
+++ src/main/java/br/ufg/inf/controller/IndexController.java
@@ -0,0 +1,14 @@
+package br.ufg.inf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "paginaInicial";
+	}
+
+}

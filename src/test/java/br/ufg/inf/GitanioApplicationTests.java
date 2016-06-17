05595cff9d1ab357e85eccd4ab0330ac53219b74
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-06-16 21:13:32 -0300

    Formatando código e adicionando .editorconfig, que irá formatar tudo como precisamos no InteliJ automaticamente.

--- src/test/java/br/ufg/inf/GitanioApplicationTests.java
+++ src/test/java/br/ufg/inf/GitanioApplicationTests.java
@@ -11,8 +11,8 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class GitanioApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }

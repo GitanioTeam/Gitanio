2a89b04aa612119b5bc9ee3a7e38be6ac16e1c8e
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2016-06-25 18:08:31 -0300

    - Adicionado jstl  nas dependências do projeto - Implementação da funcionalidade de criar produto - Implementação da funcionalidade de listar produtos - Adicionadas novas informações na criação de produtos automática ao iniciar aplicação

--- src/main/java/br/ufg/inf/controller/produto/ProdutoController.java
+++ src/main/java/br/ufg/inf/controller/produto/ProdutoController.java
@@ -1,19 +1,61 @@
 package br.ufg.inf.controller.produto;
 
+import br.ufg.inf.model.Categoria;
+import br.ufg.inf.model.Produto;
+import br.ufg.inf.repository.CategoriaRepository;
+import br.ufg.inf.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
     @RequestMapping("/listarProdutos")
-    public String listarProdutos() {
+    public String listarProdutos(Model model) {
+
+        //TODO: implementar paginação
+        Iterable<Produto> listaProdutos = produtoRepository.findAll();
+
+        model.addAttribute("listaProdutos", listaProdutos);
         return "produto/listagemProdutos";
     }
 
     @RequestMapping("/criaProduto")
-    public String produto() {
+    public String produto(Model model) {
+
+        Iterable<Categoria> listaCategorias = categoriaRepository.findAll();
+
+        model.addAttribute("listaCategorias", listaCategorias);
+
         return "produto/produto";
+    }
+
+    @RequestMapping("/salvaProduto")
+    public String salvaProduto(Integer codigo, String descricao, Long idCategoria,
+                               Double valorUnitario, Integer quantidadeMinima, Model model) {
+
+        Categoria categoria = categoriaRepository.findOne(idCategoria);
+
+        Produto produto = new Produto(
+            codigo,
+            descricao,
+            valorUnitario,
+            quantidadeMinima,
+            categoria
+        );
+        produtoRepository.save(produto);
+
+        Iterable<Produto> listaProdutos = produtoRepository.findAll();
+        model.addAttribute("listaProdutos", listaProdutos);
+
+        return "produto/listagemProdutos";
     }
 
 }
--- src/main/java/br/ufg/inf/repository/CategoriaRepository.java
+++ src/main/java/br/ufg/inf/repository/CategoriaRepository.java
@@ -2,8 +2,12 @@ package br.ufg.inf.repository;
 
 import br.ufg.inf.model.Categoria;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "categoria", path = "categoria")
 public interface CategoriaRepository extends PagingAndSortingRepository<Categoria, Long> {
+
+    Categoria findByNome(@Param("nome") String nome);
+
 }
